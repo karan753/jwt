87b05a20d2735447784755eb401c4a3259c6d02e
karan753 <karanbhosale753@gmail.com>
2023-02-06 20:31:45 +0530

    jwt

---
+++ errors/unauthenticated.js
@@ -0,0 +1,13 @@
+const customAPIError = require('./custom-error')
+const {StatusCodes} = require('http-status-codes') 
+
+
+class UnauthenticatedError extends customAPIError {
+    constructor(message) {
+      super(message)
+      this.statusCode = StatusCodes.UNAUTHORIZED
+      
+    }
+  }
+  
+  module.exports = UnauthenticatedError 
